b5cc79abae4ec5843eb1599b8240e4dfa7636cc4
tolyati <tolyakretsu@gmail.com>
2024-12-29 21:05:19 +0200

    Task 2 on level 1.1

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Hello, World!");
+"use strict"
+
+// Level 1.1 
+
+// Task 1
+
+// let num = prompt("Enter any number below:");
+
+// if(num > 0) {
+//     console.log("Positive");
+// } else if (num < 0) {
+//     console.log("Negative");
+// } else console.log("It`s zero, isn`t it?"); 
+
+// Task 2
+
+let str = prompt("Enter any string below:");
+
+console.log(str.length);
+
+
+
+
